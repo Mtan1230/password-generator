702742db6fd93798de4d1df56561424dab4fed27
Mtan1230 <m.tan1230@gmail.com>
2023-04-20 13:51:54 -0400

    add if-statement to generateString() to check weather the generated password contains every selected character type

--- assets/script.js
+++ assets/script.js
@@ -21,8 +21,6 @@ function writePassword() {
 
   passwordText.value = password.stringContent;
 
-  //Reset password.stringContent to empty
-  password.stringContent = "";
 }
 
 //Generate a password with a series of criteria
@@ -52,24 +50,20 @@ function setCharType () {
   if (password.inclLowercase = confirm("Please confirm whether or not to include lowercase letters.")) {
     charPool[i] = lowercaseLetters;
     i++;
-  } else {
   }
 
   if (password.inclUppercase = confirm("Please confirm whether or not to include uppercase letters.")) {
     charPool[i] = uppercaseLetters;
     i++;
-  } else {
   }
 
   if (password.inclNumeric = confirm("Please confirm whether or not to include numbers.")) {
     charPool[i] = numbers;
     i++;
-  } else {
   }
   
   if (password.inclSpecialChar = confirm("Please confirm whether or not to include special characters.")) {
     charPool[i] = specialChar;
-  } else {
   }
 
   if(password.inclLowercase || password.inclUppercase || password.inclNumeric || password.inclSpecialChar) {
@@ -82,14 +76,23 @@ function setCharType () {
 
 //Generate password string
 function generateString() {
+  //Reset password.stringContent to empty
+  password.stringContent = "";
+  let a = 0;
+  let b = 0;
+  let charTypes = []; 
   for (let i = 0; i < password.stringLength ; i++) {
-    let a = Math.floor(Math.random()*charPool.length); 
-    let b = Math.floor(Math.random()*(charPool[a].length));
-    // console.log(charPool[a][b]);
+    a = Math.floor(Math.random()*charPool.length); 
+    if (!charTypes.includes(a)) {
+      charTypes = charTypes.concat(a);
+    }
+    b = Math.floor(Math.random()*(charPool[a].length));
     password.stringContent = password.stringContent.concat(charPool[a][b]);
-    // console.log(password.stringContent);
   }
 
+  if (charTypes.length < charPool.length) {
+    generateString();
+  }
 }
 
 //End the process
@@ -99,20 +102,3 @@ function endGenerator() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// !WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-    // !WHEN prompted for the length of the password
-    // !THEN I choose a length of at least 8 characters and no more than 128 characters
-    // !WHEN asked for character types to include in the password
-    // !THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-    // !WHEN I answer each prompt
-    // !THEN my input should be validated and at least one character type should be selected
-    // !WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
