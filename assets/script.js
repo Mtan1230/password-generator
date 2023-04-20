5a45f3261b272c30c91dbd8ac023d1de71be7cc4
Mtan1230 <m.tan1230@gmail.com>
2023-04-20 00:56:35 -0400

    add function to set password character types

--- assets/script.js
+++ assets/script.js
@@ -20,19 +20,35 @@ function writePassword() {
 
 //Generate a password with a series of criteria
 function generatePassword() {
-  password.stringLength = setPasswordLen ();
+  setPasswordLen ();
+
 }
 
 //Set the length of the password
 function setPasswordLen() {
   const setLen = prompt("Please choose the length of your password (at least 8 characters, and no more than 128 characters", "Enter 8-128");
   if (setLen >= 8 && setLen <= 128) {
-    return setLen;
-  } else if (!setLen) {
-    endGenerator();
-  } else {
+    password.stringLength = setLen;
+    setCharType ();
+  } else if (setLen) {
     alert("Sorry, it's not a valid number.");
     setPasswordLen();
+  } else {
+    endGenerator();
+  }
+}
+
+//Set password character types
+function setCharType () {
+  password.inclLowercase = confirm("Please confirm whether or not to include lowercase letters.");
+  password.inclUppercase = confirm("Please confirm whether or not to include uppercase letters.");
+  password.inclNumeric = confirm("Please confirm whether or not to include numbers.");
+  password.inclSpecialChar = confirm("Please confirm whether or not to include special characters.");
+  if(password.inclLowercase || password.inclUppercase || password.inclNumeric || password.inclSpecialChar) {
+    // setPasswordContent ();
+  } else {
+    alert("Sorry, please select at least one character type.");
+    setCharType();
   }
 }
 
@@ -52,10 +68,10 @@ generateBtn.addEventListener("click", writePassword);
   // THEN I select which criteria to include in the password
     // !WHEN prompted for the length of the password
     // !THEN I choose a length of at least 8 characters and no more than 128 characters
-    // WHEN asked for character types to include in the password
-    // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+    // !WHEN asked for character types to include in the password
+    // !THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    // !WHEN I answer each prompt
+    // THEN my input should be validated and at least one character type should be selected
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
